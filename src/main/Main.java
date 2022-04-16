5759befab1c89c6bf1a02387e306040998c9d25f
Ryan Mraz <rmm0049@mix.wvu.edu>
2022-04-16 09:54:20 -0400

    New Employee adding functionality

--- src/main/Main.java
+++ src/main/Main.java
@@ -67,10 +67,99 @@ public class Main {
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			JFrame newEmployeeFrame = new JFrame("Register New Customer");
-			newEmployeeFrame.setSize(600,400);
+			JFrame newEmployeeFrame = new JFrame("Add New Employee");
+			newEmployeeFrame.setSize(850,200);
 			JPanel newEmployeePanel = new JPanel();
 			
+			JTextField fname = new JTextField(10);
+			JTextField minitial = new JTextField(1);
+			JTextField lname = new JTextField(10);
+			JTextField e_id = new JTextField(10);
+			JTextField job_title = new JTextField(10);
+			JTextField hourly_rate = new JTextField(10);
+			JTextField manager_eid = new JTextField(10);
+			
+			JLabel fname_label = new JLabel("First Name");
+			JLabel minitial_label = new JLabel("Middle Initial");
+			JLabel lname_label = new JLabel("Last Name");
+			JLabel e_id_label = new JLabel("Employee ID");
+			JLabel job_title_label = new JLabel("Job Title");
+			JLabel hourly_rate_label = new JLabel("Hourly Rate");
+			JLabel manager_eid_label = new JLabel("Manager EID");
+			
+			JButton submit = new JButton("Submit");
+			
+			newEmployeePanel.add(fname_label);
+			newEmployeePanel.add(fname);
+			newEmployeePanel.add(minitial_label);
+			newEmployeePanel.add(minitial);
+			newEmployeePanel.add(lname_label);
+			newEmployeePanel.add(lname);
+			newEmployeePanel.add(e_id_label);
+			newEmployeePanel.add(e_id);
+			newEmployeePanel.add(job_title_label);
+			newEmployeePanel.add(job_title);
+			newEmployeePanel.add(hourly_rate_label);
+			newEmployeePanel.add(hourly_rate);
+			newEmployeePanel.add(manager_eid_label);
+			newEmployeePanel.add(manager_eid);
+		
+			newEmployeePanel.add(submit);
+			
+			ActionListener submitNewEmployee = new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					//Submit INSERT query for new customer
+					String query = "INSERT INTO EMPLOYEES\r\n" + 
+							"VALUES (?,?,?,?,?,?,?)";
+					
+					Connection con;
+					try {
+						con = DriverManager.getConnection(JDBC_URL, "TEAM_ALPHA", "875283");
+						PreparedStatement prep = con.prepareStatement(query);
+						
+						
+						if (fname.getText().isEmpty()
+								|| lname.getText().isEmpty()
+								|| e_id.getText().isEmpty()
+								|| job_title.getText().isEmpty()
+								|| manager_eid.getText().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "Last Name, e_id, job title and hourly rate cannot be null!","Error", JOptionPane.PLAIN_MESSAGE);
+						} else {
+							prep.setString(1, fname.getText());
+							prep.setString(2, minitial.getText());
+							prep.setString(3, lname.getText());
+							prep.setString(4, e_id.getText());
+							prep.setString(5, job_title.getText());
+							prep.setString(6, hourly_rate.getText());
+							prep.setString(7, manager_eid.getText());
+
+							
+							try {
+								final int count = prep.executeUpdate();
+								newEmployeeFrame.dispose();
+								JOptionPane.showMessageDialog(null, "Successfully added new employee!","Success", JOptionPane.PLAIN_MESSAGE);
+							}
+							catch (SQLException sqle) {
+								System.err.println(sqle.getMessage());
+							}
+							
+						}
+						
+						
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+					
+				}
+			};
+			
+			
+			submit.addActionListener(submitNewEmployee);
+			
+			newEmployeeFrame.add(newEmployeePanel);
 			newEmployeeFrame.setVisible(true);
 		}
 	};
@@ -79,7 +168,7 @@ public class Main {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			JFrame registerCustomerFrame = new JFrame("Register New Customer");
-			registerCustomerFrame.setSize(600,400);
+			registerCustomerFrame.setSize(850,200);
 			JPanel registerCustomerPanel = new JPanel();
 			
 			// Text fields
